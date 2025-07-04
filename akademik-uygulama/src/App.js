ad9b8efe487745539cbbf1c32290d0c07c1744cf
gamze <gmznurak@gmail.com>
2025-07-04 15:52:56 +0300

    gitpages için düzenleme

--- akademik-uygulama/src/App.js
+++ akademik-uygulama/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom'; // 👈 DEĞİŞTİ
 import Dashboard from './pages/Dashboard';
 import Login from './pages/login'; 
 import Register from './pages/register';
